e36891a8dca993042da44770b3d4b536369fa32c
Sidharth Babu <sidharthb@greenshiptech.com>
2026-01-10 17:29:10 +0530

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10443167
+    "domain": "3nbf4.com",
+    "zoneId": 10443348
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
